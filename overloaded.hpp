9a19893927f9d192936e92ea529cdcf4cd92f834
Max Charlington <36207528+MaxCharlington@users.noreply.github.com>
2022-10-25 19:03:06 +0300

    Create overloaded.hpp

---
+++ overloaded.hpp
@@ -0,0 +1,13 @@
+#ifndef VARIANT_HELPER_H
+#define VARIANT_HELPER_H
+
+template <class... Ts>
+struct overloaded : Ts...
+{
+    using Ts::operator()...;
+};
+
+template <class... Ts>
+overloaded(Ts...) -> overloaded<Ts...>;
+
+#endif
