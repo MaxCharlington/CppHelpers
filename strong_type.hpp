e9d1b7fa269dbdaf215bee4d7fb166723022312e
Max Charlington <36207528+MaxCharlington@users.noreply.github.com>
2022-10-25 18:10:21 +0300

    Create strong_type.hpp

---
+++ strong_type.hpp
@@ -0,0 +1,11 @@
+template<typename UnderlingType, typename Tag>
+class StrongType
+{
+public:
+    explicit StrongType(const UnderlingType& val) : m_val{val} {}
+    UnderlingType get() const { return m_val; }
+    bool operator==(const StrongType<UnderlingType, Tag>& other) const { return this->m_val == other.m_val; }
+
+private:
+    UnderlingType m_val;
+};
